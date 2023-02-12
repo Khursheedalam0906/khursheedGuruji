d0df26ccde9a880e260144b1e2db4e5ac5d90c87
Khursheed Alam <khursheedalam0906@gmail.com>
2023-02-12 10:47:00 +0530

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const fetchBotResponse = async () => {
     const { data } = await axios.post(
-      "http://localhost:4000",
+      "https://khursheedguruji.onrender.com",
       { input },
       {
         headers: {
